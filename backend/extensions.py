5082f9a95288a394642a0c0dbbb24c71a381008b
Sajim Ahmed <sajimahmed73@gmail.com>
2026-04-15 23:25:11 -0500

    Replace JWTManager with Bcrypt in extensions.py
    Replaced JWTManager with Bcrypt for password hashing.

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,5 +1,5 @@
-﻿from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
-jwt = JWTManager()
+bcrypt = Bcrypt()
