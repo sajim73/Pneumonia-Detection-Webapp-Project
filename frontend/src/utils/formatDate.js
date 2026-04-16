ffb9044ce456cab72d215de7ff6056765804dbc1
Sajim Ahmed <sajimahmed73@gmail.com>
2026-04-16 00:29:11 -0500

    Refactor formatDate function and remove formatPercent

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export function formatDate(value) {
+  if (!value) return "N/A";
+
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return "N/A";
+
+  return date.toLocaleString();
+}
